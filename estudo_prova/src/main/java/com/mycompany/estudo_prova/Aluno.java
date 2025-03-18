a440dbbe19f91fe0821c90065bd3cdd462974e1e
eKaruii <159392951+eKaruii@users.noreply.github.com>
2025-03-18 10:59:26 -0300

    Add files via upload

---
+++ estudo_prova/src/main/java/com/mycompany/estudo_prova/Aluno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.estudo_prova;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class Aluno {
+    
+}
---
+++ estudo_prova/src/main/java/com/mycompany/estudo_prova/Estudo_prova.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estudo_prova;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class Estudo_prova {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
